a60b0073f2c4493ca50eefac33880ef251f39814
Robby Ray <rgr5035@gmail.com>
2020-10-22 14:58:29 -0600

    troubleshooting ID grabs

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,35 @@
 //variable declarations stored in global memory
-
 var dateDisplay = document.getElementById('currentDay');
-var timeColor = document.getElementById('text-color');
+var timeColor = document.querySelectorAll('#text-color');
+var saveBtnEl = document.getElementById('saveBtn');
+var docText = document.querySelector('.description');
 
 //displays the current date and time
 dateDisplay.textContent = moment().format('LLLL');
 
-function changeTimingColors () {
+function setColor() {
+timeColor.setAttribute("class", "future");
+}
+ setColor();
 
-if (moment().isSame('LLLL')) {
-    timeColor.setAttribute("class", "present");
-} else if (moment().isBefore('LLLL')) {
-    timeColor.setAttribute("class", "past");
-} else if (moment().isAfter('LLLL')) {
-    timeColor.setAttribute("class", "future");
-}}
+//function to run code block of changing background color of text area based on the time
+// function changeTimingColors () {
 
-changeTimingColors();
+// if (moment().isSame('hour')) {
+//     timeColor.setAttribute("class", "present");
+// } else if (moment().isBefore('hour')) {
+//     timeColor.setAttribute("class", "past");
+// } else if (moment().isAfter('hour')) {
+//     timeColor.setAttribute("class", "future");
+// }}
+
+// changeTimingColors();
+
+
+//event handler for 'save' button to store text in local storage
+saveBtnEl.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('Im clicked');
+
+    // var savedInput = 
+})
