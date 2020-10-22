87c6e1fbd72565428d0cf8c78343b71d24c15660
Robby Ray <rgr5035@gmail.com>
2020-10-22 11:42:07 -0600

    added styling to HTML rows and working on time color changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,15 @@
 //variable declarations stored in global memory
 
 var dateDisplay = document.getElementById('currentDay');
-
+var timeColor = document.getElementById('text-color');
 
 //displays the current date and time
 dateDisplay.textContent = moment().format('LLLL');
 
+function changeTimingColors () {
+
+if (moment().isSame('LLLL')) {
+    timeColor.setAttribute("class", "present");
+} }
+
+changeTimingColors();
