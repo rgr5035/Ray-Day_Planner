92bdea474c17a759e939cd03dd639e262ed9d329
Robby Ray <rgr5035@gmail.com>
2020-10-23 17:54:41 -0600

    attempting to store local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,5 @@
 //variable declarations stored in global memory
 var dateDisplay = document.getElementById('currentDay');
-var timeColor = document.querySelectorAll('#text-color');
-var saveBtnEl = document.getElementById('saveBtn');
+var timeColor = document.querySelectorAll('textarea');
+var saveBtnEl = document.querySelectorAll('button');
 var docText = document.querySelector('.description');
-
-//displays the current date and time
-dateDisplay.textContent = moment().format('LLLL');
-
-function setColor() {
-timeColor.setAttribute("class", "future");
-}
- setColor();
-
-//function to run code block of changing background color of text area based on the time
-// function changeTimingColors () {
-
-// if (moment().isSame('hour')) {
-//     timeColor.setAttribute("class", "present");
-// } else if (moment().isBefore('hour')) {
-//     timeColor.setAttribute("class", "past");
-// } else if (moment().isAfter('hour')) {
-//     timeColor.setAttribute("class", "future");
-// }}
-
-// changeTimingColors();
-
-
-//event handler for 'save' button to store text in local storage
-saveBtnEl.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('Im clicked');
-
-    // var savedInput = 
-})
