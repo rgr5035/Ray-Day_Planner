f48d8480005713953245336f86cee2fe418457ca
Robby Ray <rgr5035@gmail.com>
2020-10-24 09:46:06 -0600

    moved jQuery code to external JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,49 @@
-//variable declarations stored in global memory
-var dateDisplay = document.getElementById('currentDay');
-var timeColor = document.querySelectorAll('textarea');
-var saveBtnEl = document.querySelectorAll('button');
-var docText = document.querySelector('.description');
+
+
+$('#currentDay').html(moment().format('LLLL'));
+
+$('.description').each(function(index, value) {
+        
+
+    var currentHour = $(this).attr("data-hour");
+        
+    if (currentHour < moment().hour()) {
+        $(this).addClass("past");
+    } else if (currentHour == moment().hour()) {
+        $(this).addClass("present");
+    } else if (currentHour > moment().hour()) {
+        $(this).addClass("future");
+    
+    }
+        //JSON parse, look to see if I need this                                                                                                              
+    })
+
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+      // $(".description").each(function ()  {
+
+      //   var hourNUm = `hour-${$(this).attr("data-hour")}`
+
+      //   $(this).val(localStorage.getItem(hourNum));
+
+      // })
+
+$('.saveBtn').on('click', function(e) {
+      
+    e.preventDefault();
+    
+    var textInput = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    
+    localStorage.setItem(hour, textInput);
+
+});
